1480899c0971a3190f173bd7a7b5eb818d461e74
danisluis6 <vuongluis@hotmail.com>
2017-01-04 15:23:18 +0700

    ok

--- SourceCode/app/src/main/java/com/example/enclaveit/appuser/MainActivity.java
+++ SourceCode/app/src/main/java/com/example/enclaveit/appuser/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.enclaveit.appuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email,pass;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_layout_login);
+        setContentView(R.layout.ui_login);
+        initComponents();
+    }
+
+    private void initComponents() {
+        email = (EditText)this.findViewById(R.id.email);
+        pass = (EditText)this.findViewById(R.id.pass);
     }
 }
